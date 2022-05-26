ec8960ee68ae5bd90debdc0a0f2f59a64d459bd9
David Emilio Iriondo Palacios <davidiriondopalacios@gmail.com>
2022-05-26 17:28:53 +0200

    Packages can be added and deleted from UI
    The implementation count with a small function which doesn´t allow to add one package twice, but this function can be enhance
    
    This function add a class named pkg-selected to our card in html, and removed when this card is removed and allow it to be selected again

--- flash/src/main/resources/static/js/flask-packages/main.js
+++ flash/src/main/resources/static/js/flask-packages/main.js
@@ -1,49 +1,5 @@
 import {packageList} from "./package-list.js"
 
 $(document).ready(function () {
-
-  //get data
   packageList();
-
-  //PACKAGE OPTION
-  $("#package-option-1").click(function () {
-    console.log("Has seleccionado el paquete de flask 1");
-
-    //Descativate option
-    $("#package-option-1").parent().removeClass("card-hover");
-    //Add desactivate style
-    $("#package-option-1").parent().addClass("card-desactivated");
-
-    //Add option to packages container
-    $("#packages-container").append(
-      `<div class="card shadow-sm card-selected card-option">
-      <div class="card-body" id="package-selected-1">
-          <h5 class="card-title">Flask package 1</h5>
-          <div class="d-flex justify-content-between">
-              <p class="card-text mr-2 text-truncate">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                   Ut vestibulum vitae arcu vitae commodo. Aliquam felis metus, venenatis aliquam mi in,
-                    tristique suscipit orci
-              </p>
-          </div>
-      </div>
-  </div>`
-    )})
-
-  //PACKAGE SELECTED
-  $("#packages-container").on('click', "#package-selected-1",  function name() {
-
-    console.log("Has eliminado el paquete de flask 1")
-  
-    //Eliminate package div
-    $(this).parent().remove();
-
-
-    //Descativate option
-    $(this).parent().removeClass("card-desactivated");
-    //Add desactivate style
-    $(this).parent().addClass("card-hover");
-
-  })
-  
 });
--- flash/target/classes/static/js/flask-packages/package-list.js
+++ flash/target/classes/static/js/flask-packages/package-list.js
@@ -10,6 +10,7 @@ export function packageList(){
   .then(r => {
     console.log(r.data)
     printPackages(r.data)
+    printSelectedPackages(r.data)
   })
   .catch(e => {console.log(e)})
 }   
@@ -19,7 +20,7 @@ function printPackages(packageList) {
   console.log("CARGANDO LOS DATOS")
   packageList.forEach(element => {
           $("#packages-options-container").append(
-            `<div class="card shadow-sm card-hover mt-1 ">
+            `<div class="card shadow-sm card-hover mt-1 " id="package-option-${element.id}">
             <div class="card-body">
                 <h5 class="card-title">${element.name}</h5>
                 <div class="d-flex justify-content-between">
@@ -31,4 +32,73 @@ function printPackages(packageList) {
         </div>`
           )
   });
+}
+
+function printSelectedPackages(packageList) {
+  
+  packageList.forEach(element => {
+    let packageID = "#package-option-"+element.id;
+    let packageName = element.name;
+    let packageDescription = element.description;
+
+    //PACKAGES OPTIONS
+    $("#packages-options-container").on('click', packageID,  function name() {
+    console.log("Has seleccionado el paquete " + packageName);
+
+    //Add desactivate style
+    $(packageID).removeClass("card-hover");
+    $(packageID).addClass("card-desactivated");  
+    
+    //If this package has not been added to selected packages container then it does
+    if(!$(packageID).hasClass("pkg-selected")){
+        //Add option to packages container
+        $("#packages-container").append(
+          `<div class="card shadow-sm">
+          <div class="card-body" id="package-selected-${element.id}">
+              <h5 class="card-title">${packageName}</h5>
+              <div class="d-flex justify-content-between">
+                  <p class="card-text mr-2 mb-0 text-truncate">
+                    ${packageDescription}
+                  </p>
+                  <p class="mb-0" id="dlt-btn-${element.id}">
+                      <i class="fas fa-times-circle red-flash-c btn-pkg-delete"></i>
+                  </p>
+              </div>
+          </div>
+      </div>   `
+        )
+        
+    //Add class to know if has been selected or not 
+    $(packageID).addClass("pkg-selected");
+    }else{
+      alert("Package already added :)")
+    }
+   })
+
+    let packageSelecteId = "#package-selected-" + element.id;
+    let deleteButtonId = "#dlt-btn-" + element.id;
+
+    //PACKAGES SELECTED
+    $("#packages-container").on('click', deleteButtonId,  function name() {
+  
+      console.log("Has eliminado el paquete de " + packageName);
+    
+      //Eliminate package div
+      $(packageSelecteId).parent().remove();
+  
+  
+      //Descativate option
+      $(packageID).removeClass("card-desactivated");
+      //Add desactivate style
+      $(packageID).addClass("card-hover");
+      //Remove boolean class
+      $(packageID).removeClass("pkg-selected");
+  
+    })
+
+
+
+  });
+
+  
 }
